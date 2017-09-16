dc23bf724bd05988814f664036a4d94c4532ea40
Linming <1438943646@qq.com>
2017-09-16 15:31:29 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,45 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by lenovo on 2017/9/16.
+ */
+public class City extends DataSupport {
+    private int id;
+    private String cityName;//j记录市的名字
+    private int cityCode;//记录市的代号
+    private int provinceId;//记录当前市所属省的id值
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,37 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by lenovo on 2017/9/16.
+ */
+//province类用来存放省份信息
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;//记录省的名字
+    private int provinceCode;//记录省的代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
